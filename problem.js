c2d912d26c9f7cbe5e239cbc128ac19e9fbb8bef
Rocko <codingwithrock@gmail.com>
2022-08-26 00:36:02 +0600

    problem one done

---
+++ problem.js
@@ -0,0 +1,12 @@
+// !problem 1
+/* 1) Write an arrow function that will take 3 parameters, will multiply
+the parameters and will return the result. */ 
+
+const totalCost = (apple, banana,mango)=> apple * banana * mango;
+
+console.log(totalCost(10,12,12));
+
+
+g
+
+
